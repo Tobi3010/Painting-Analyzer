d4055ff65bd7fddc6479a548220eca2b513ef6a4
Tobi3010 <tobiasandersen62@gmail.com>
2025-07-02 21:37:50 +0200

    basics

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QTextStream>
+#include <QPixmap>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("C:/Dev/Painting-Analyzer/images/carla4.jpg");
+
+    QSize picSize = ui->picture->size();
+    ui->picture->setPixmap(pix.scaled(picSize.width(), picSize.height(), Qt::KeepAspectRatio));
+
+
+    QString sizeString = QString("(%1,%2)").arg(picSize.width()).arg(picSize.height());
+    QTextStream out(stdout);
+    out << sizeString;
 }
 
 MainWindow::~MainWindow()
